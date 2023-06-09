271deceb2622b0a4f9fddf07b36c337f276efa56
satrio5g <136000267+satrio5g@users.noreply.github.com>
2023-06-09 19:07:01 +0700

    Add files via upload

---
+++ localhost-homepage-html/js/vw-education-academy-assets-js-custom.js
@@ -0,0 +1,140 @@
+function vw_education_academy_menu_open_nav() {
+	window.vw_education_academy_responsiveMenu=true;
+	jQuery(".sidenav").addClass('show');
+}
+function vw_education_academy_menu_close_nav() {
+	window.vw_education_academy_responsiveMenu=false;
+ 	jQuery(".sidenav").removeClass('show');
+}
+
+jQuery(function($){
+ 	"use strict";
+   	jQuery('.main-menu > ul').superfish({
+		delay:       500,
+		animation:   {opacity:'show',height:'show'},
+		speed:       'fast'
+   	});
+});
+
+jQuery(document).ready(function () {
+	window.vw_education_academy_currentfocus=null;
+  	vw_education_academy_checkfocusdElement();
+	var vw_education_academy_body = document.querySelector('body');
+	vw_education_academy_body.addEventListener('keyup', vw_education_academy_check_tab_press);
+	var vw_education_academy_gotoHome = false;
+	var vw_education_academy_gotoClose = false;
+	window.vw_education_academy_responsiveMenu=false;
+ 	function vw_education_academy_checkfocusdElement(){
+	 	if(window.vw_education_academy_currentfocus=document.activeElement.className){
+		 	window.vw_education_academy_currentfocus=document.activeElement.className;
+	 	}
+ 	}
+ 	function vw_education_academy_check_tab_press(e) {
+		"use strict";
+		// pick passed event or global event object if passed one is empty
+		e = e || event;
+		var activeElement;
+
+		if(window.innerWidth < 999){
+		if (e.keyCode == 9) {
+			if(window.vw_education_academy_responsiveMenu){
+			if (!e.shiftKey) {
+				if(vw_education_academy_gotoHome) {
+					jQuery( ".main-menu ul:first li:first a:first-child" ).focus();
+				}
+			}
+			if (jQuery("a.closebtn.mobile-menu").is(":focus")) {
+				vw_education_academy_gotoHome = true;
+			} else {
+				vw_education_academy_gotoHome = false;
+			}
+
+		}else{
+
+			if(window.vw_education_academy_currentfocus=="responsivetoggle"){
+				jQuery( "" ).focus();
+			}}}
+		}
+		if (e.shiftKey && e.keyCode == 9) {
+		if(window.innerWidth < 999){
+			if(window.vw_education_academy_currentfocus=="header-search"){
+				jQuery(".responsivetoggle").focus();
+			}else{
+				if(window.vw_education_academy_responsiveMenu){
+				if(vw_education_academy_gotoClose){
+					jQuery("a.closebtn.mobile-menu").focus();
+				}
+				if (jQuery( ".main-menu ul:first li:first a:first-child" ).is(":focus")) {
+					vw_education_academy_gotoClose = true;
+				} else {
+					vw_education_academy_gotoClose = false;
+				}			
+			}else{
+
+			if(window.vw_education_academy_responsiveMenu){
+			}}}}
+		}
+	 	vw_education_academy_checkfocusdElement();
+	}
+});
+
+(function( $ ) {
+	jQuery('document').ready(function($){
+	    setTimeout(function () {
+    		jQuery("#preloader").fadeOut("slow");
+	    },1000);
+	});
+
+	$(document).ready(function () {
+		$(window).scroll(function () {
+		    if ($(this).scrollTop() > 100) {
+		        $('.scrollup i').fadeIn();
+		    } else {
+		        $('.scrollup i').fadeOut();
+		    }
+		});
+		$('.scrollup i').click(function () {
+		    $("html, body").animate({
+		        scrollTop: 0
+		    }, 600);
+		    return false;
+		});
+	});	
+})( jQuery );
+
+jQuery(document).ready(function () {
+  	function vw_education_academy_search_loop_focus(element) {
+	  var vw_education_academy_focus = element.find('select, input, textarea, button, a[href]');
+	  var vw_education_academy_firstFocus = vw_education_academy_focus[0];  
+	  var vw_education_academy_lastFocus = vw_education_academy_focus[vw_education_academy_focus.length - 1];
+	  var KEYCODE_TAB = 9;
+
+	  element.on('keydown', function vw_education_academy_search_loop_focus(e) {
+	    var isTabPressed = (e.key === 'Tab' || e.keyCode === KEYCODE_TAB);
+
+	    if (!isTabPressed) { 
+	      return; 
+	    }
+
+	    if ( e.shiftKey ) /* shift + tab */ {
+	      if (document.activeElement === vw_education_academy_firstFocus) {
+	        vw_education_academy_lastFocus.focus();
+	          e.preventDefault();
+	        }
+	      } else /* tab */ {
+	      if (document.activeElement === vw_education_academy_lastFocus) {
+	        vw_education_academy_firstFocus.focus();
+	          e.preventDefault();
+	        }
+	      }
+	  });
+	}
+	jQuery('.search-box span a').click(function(){
+        jQuery(".serach_outer").slideDown(1000);
+    	vw_education_academy_search_loop_focus(jQuery('.serach_outer'));
+    });
+
+    jQuery('.closepop a').click(function(){
+        jQuery(".serach_outer").slideUp(1000);
+    });
+});
